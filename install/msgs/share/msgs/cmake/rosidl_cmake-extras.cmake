cfd7ac64e74895fd0669ac3a9ed9733451c09517
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-03-08 19:00:44 -0600

    Delete install directory

--- install/msgs/share/msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(msgs_IDL_FILES "msg/SignalParams.idl;msg/Sphere.idl")
-set(msgs_INTERFACE_FILES "msg/SignalParams.msg;msg/Sphere.msg")
