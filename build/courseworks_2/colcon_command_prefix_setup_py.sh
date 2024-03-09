f54783840de0452e3a6c28e383ffce6be0300909
Rodrigo Llaguno <rodrigollagunoc@gmail.com>
2024-03-08 19:00:57 -0600

    Delete build directory

--- build/courseworks_2/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/ros2_ws/install/msgs/share/msgs/package.sh"
--- build/msgs/rosidl_generator_py/msgs/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from msgs.msg._signal_params import SignalParams  # noqa: F401
-from msgs.msg._sphere import Sphere  # noqa: F401
